c5a57daf4c451a81bedbf3d3f14a8bfd5bc6ab93
David Gordon <gordo893@msi.umn.edu>
2023-11-06 15:14:19 -0600

    run_snakemake.sh submit to cluster

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -3,4 +3,4 @@
 conda activate snakemake
 
 
-snakemake -s ideogram_of_kmers_found.snake -j 2
+snakemake -s ideogram_of_kmers_found.snake --jobname "{rulename}.{jobid}" --profile profile -w 60 --jobs 50 -p -k
